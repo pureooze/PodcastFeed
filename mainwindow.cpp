c6f0f5f584a9e49a9b147a40a4515869edaf60af
ForeEyes <vamsiazure@gmail.com>
2017-03-19 18:03:17 -0400

    Add Podcast Dialog implemented
    Custom sized input dialog for getting itunes link from the user.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,6 +17,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionUsing_Itunes_Link_triggered()
 {
+    //Bool to tell if user clicked ok on dialog.
+    bool ok;
+    //call the Podcast dialog function and pass the label, and window title
+    QString Itunes_Link = addPodcast_dlg("Itunes Link:", "Add Podcast using Itunes Link", ok);
+    //Variable for storing rss link.
+    QString rssLink;
+    //Check if user clicked ok and it the string is empty
+    if(ok && !Itunes_Link.isEmpty()){
+        //Pass Itunes Link & Reference of FeedURL to getRSSurl
+        //function return true if it was able to get the rss link
+        if(getRSSurl(Itunes_Link, rssLink)){
+
+        }
+
+    }
+
 
 }
 
@@ -33,4 +49,30 @@ void MainWindow::on_actionRefresh_Feed_triggered()
 void MainWindow::on_actionRemove_Podcast_triggered()
 {
 
+}
+
+//Custom Dialog for Adding Podcast
+QString MainWindow::addPodcast_dlg(QString Label, QString Title, bool &ok){
+    //Create New Dialog
+    QInputDialog *dlg = new QInputDialog();
+    //Set the input as text input
+    dlg->setInputMode(QInputDialog::TextInput);
+    //Label for text input
+    dlg->setLabelText(Label);
+    //Label for Dialog
+    dlg->setWindowTitle(Title);
+    //Set Dialog Size
+    dlg->resize(600,500);
+    //Exec Dialog and return true is user clicks ok, else return false
+    //Store this value in bool reference ok.
+    ok = dlg->exec();
+    //Return the Text From the Text Input
+    return dlg->textValue();
+}
+
+
+//Take Itunes Link, make http request, get json reply
+//Parse json reply and find the rss link for podcast
+bool MainWindow::getRSSurl(QString itunesLink,QString &rssLink){
+    return true;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QInputDialog>
 
 namespace Ui {
 class MainWindow;
@@ -16,6 +17,7 @@ public:
     ~MainWindow();
 
 private slots:
+
     void on_actionUsing_Itunes_Link_triggered();
 
     void on_actionUsing_RSS_Link_triggered();
@@ -23,6 +25,10 @@ private slots:
     void on_actionRefresh_Feed_triggered();
 
     void on_actionRemove_Podcast_triggered();
+
+    QString addPodcast_dlg(QString Label, QString Title, bool &ok);
+
+    bool getRSSurl(QString itunesLink,QString &rssLink);
 
 private:
     Ui::MainWindow *ui;
